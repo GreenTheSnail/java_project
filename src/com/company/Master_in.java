411e88493601f6139573ac7936edb31dbadf6b8a
GreenTheSnail <soshkovladislav@gmail.com>
2020-11-26 18:54:56 +0100

    +try catch

--- src/com/company/Master_in.java
+++ src/com/company/Master_in.java
@@ -4,62 +4,67 @@ import java.util.Scanner;
 
 public class Master_in {
     private int x, y, z, t;
-    private boolean isNumber, isNumber2, isNumber3, isNumber4; //booleans for input loop
+    private boolean isNumber1, isNumber2, isNumber3, isNumber4; //booleans for input loop
+    private String str1, str2, str3, str4; //strings to delete spaces
     public void input() {
         System.out.println("Výška úvěru:");
         Scanner obj1 = new Scanner(System.in);
         do {
-            if (obj1.hasNextInt()) {
-                x = obj1.nextInt();
-                isNumber = true;
-            }
-            else{
-                System.out.println("Výška úvěru musí obsahovat jenom čísla bez mezer. Zkuste ještě jednou:");
-                isNumber = false;
-                obj1.next();
-            }
-        } while (!(isNumber));
+                str1 = obj1.nextLine();
+                str1 = str1.replaceAll(" ","");
+                isNumber1 = true;
+                try {
+                    x = Integer.parseInt(str1);
+
+                } catch (NumberFormatException | NullPointerException nfe) {
+                    System.out.println("Výška úvěru musí obsahovat jenom čísla. Zkuste ještě jednou:");
+                    isNumber1 = false;
+                }
+        } while (!(isNumber1));
 
         System.out.println("Úroková míra:");
         Scanner obj2 = new Scanner(System.in);
+
         do {
-            if (obj2.hasNextInt()) {
-                y = obj2.nextInt();
-                isNumber2 = true;
-            }
-            else {
-                System.out.println("Úroková míra musí obsahovat jenom čísla bez mezer. Zkuste ještě jednou:");
+            str2 = obj2.nextLine();
+            str2 = str2.replaceAll(" ","");
+            isNumber2 = true;
+            try {
+                y = Integer.parseInt(str2);
+
+            } catch (NumberFormatException | NullPointerException nfe) {
+                System.out.println("Úroková míra musí obsahovat jenom čísla. Zkuste ještě jednou:");
                 isNumber2 = false;
-                obj2.next();
             }
         } while (!(isNumber2));
 
         System.out.println("Doba splácení:");
         Scanner obj3 = new Scanner(System.in);
         do {
-            if (obj3.hasNextInt()) {
-                z = obj3.nextInt();
-                isNumber3 = true;
-            }
-            else {
-                System.out.println("Doba splácení musí obsahovat jenom čísla bez mezer. Zkuste ještě jednou:");
+            str3 = obj3.nextLine();
+            str3 = str3.replaceAll(" ","");
+            isNumber3 = true;
+            try {
+                z = Integer.parseInt(str3);
+
+            } catch (NumberFormatException | NullPointerException nfe) {
+                System.out.println("Doba splácení musí obsahovat jenom čísla. Zkuste ještě jednou:");
                 isNumber3 = false;
-                obj3.next();
             }
         } while (!(isNumber3));
 
         System.out.println("Interval:");
         Scanner obj4 = new Scanner(System.in);
         do {
-            if (obj4.hasNextInt()) {
-                t = obj4.nextInt();
-                isNumber4 = true;
+            str4 = obj4.nextLine();
+            str4 = str4.replaceAll(" ","");
+            isNumber4 = true;
+            try {
+                t = Integer.parseInt(str4);
 
-            }
-            else{
-                System.out.println("Interval musí obsahovat jenom čísla bez mezer. Zkuste ještě jednou:");
+            } catch (NumberFormatException | NullPointerException nfe) {
+                System.out.println("Interval musí obsahovat jenom čísla. Zkuste ještě jednou:");
                 isNumber4 = false;
-                obj4.next();
             }
         } while (!(isNumber4));
     }
