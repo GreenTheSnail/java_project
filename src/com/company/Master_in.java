71f276a1108983c865464808cb774f1a01452549
GreenTheSnail <soshkovladislav@gmail.com>
2020-11-26 16:43:19 +0100

    validation

--- src/com/company/Master_in.java
+++ src/com/company/Master_in.java
@@ -4,19 +4,64 @@ import java.util.Scanner;
 
 public class Master_in {
     private int x, y, z, t;
+    private boolean isNumber, isNumber2, isNumber3, isNumber4; //booleans for input loop
     public void input() {
         System.out.println("Výška úvěru:");
         Scanner obj1 = new Scanner(System.in);
-        x = obj1.nextInt();
+        do {
+            if (obj1.hasNextInt()) {
+                x = obj1.nextInt();
+                isNumber = true;
+            }
+            else{
+                System.out.println("Výška úvěru musí obsahovat jenom čísla bez mezer. Zkuste ještě jednou:");
+                isNumber = false;
+                obj1.next();
+            }
+        } while (!(isNumber));
+
         System.out.println("Úroková míra:");
         Scanner obj2 = new Scanner(System.in);
-        y = obj2.nextInt();
+        do {
+            if (obj2.hasNextInt()) {
+                y = obj2.nextInt();
+                isNumber2 = true;
+            }
+            else {
+                System.out.println("Úroková míra musí obsahovat jenom čísla bez mezer. Zkuste ještě jednou:");
+                isNumber2 = false;
+                obj2.next();
+            }
+        } while (!(isNumber2));
+
         System.out.println("Doba splácení:");
         Scanner obj3 = new Scanner(System.in);
-        z = obj3.nextInt();
+        do {
+            if (obj3.hasNextInt()) {
+                z = obj3.nextInt();
+                isNumber3 = true;
+            }
+            else {
+                System.out.println("Doba splácení musí obsahovat jenom čísla bez mezer. Zkuste ještě jednou:");
+                isNumber3 = false;
+                obj3.next();
+            }
+        } while (!(isNumber3));
+
         System.out.println("Interval:");
         Scanner obj4 = new Scanner(System.in);
-        t = obj4.nextInt();
+        do {
+            if (obj4.hasNextInt()) {
+                t = obj4.nextInt();
+                isNumber4 = true;
+
+            }
+            else{
+                System.out.println("Interval musí obsahovat jenom čísla bez mezer. Zkuste ještě jednou:");
+                isNumber4 = false;
+                obj4.next();
+            }
+        } while (!(isNumber4));
     }
 
             public int vyska_uveru () {
